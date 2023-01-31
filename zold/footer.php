f3979c417dea8335a6025596627e87ab6f335554
Xc3l <wangceline200141@yahoo.fr>
2023-01-31 19:43:28 +0100

    init

--- zold/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <a href="#" target="_blank" alt="Linkedin"><img src="/img/png/linkedin.png" id="linkedin"></a>
-    <a href="#" target="_blank" alt="Mail"><img src="/img/png/mail.png" id="mail"></a>
-    <a href="javascript:" id="return-to-top"><img src="/img/png/arrow.png"></a>
-</footer>
